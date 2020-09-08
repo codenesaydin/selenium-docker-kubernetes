aba19c5284f134287dff5830a14e2fe3d160696e
codenesaydin <enes.aydin@sahibinden.com>
2020-09-08 11:47:30 +0300

    docker-compose.yml

--- src/main/java/com/base/BaseTest.java
+++ src/main/java/com/base/BaseTest.java
@@ -1,12 +1,17 @@
 package com.base;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -20,7 +25,7 @@ public class BaseTest
     {
         System.setProperty("webdriver.chrome.driver", "/opt/chromedriver");
         ChromeOptions options = new ChromeOptions();
-        options.setHeadless(true);
+        //options.setHeadless(true);
 
         driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
         driver.manage().window().maximize();
@@ -33,5 +38,16 @@ public class BaseTest
     public void teardown()
     {
         driver.quit();
+    }
+
+    public void takeSnapShot(WebDriver webdriver, String fileWithPath) throws IOException
+    {
+        TakesScreenshot scrShot = ((TakesScreenshot) webdriver);
+
+        File SrcFile = scrShot.getScreenshotAs(OutputType.FILE);
+
+        File DestFile = new File(fileWithPath);
+
+        FileUtils.copyFile(SrcFile, DestFile);
     }
 }
--- src/test/java/com/SahibindenDemo.java
+++ src/test/java/com/SahibindenDemo.java
@@ -6,55 +6,69 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
 
+import java.io.IOException;
+
 @Execution(ExecutionMode.CONCURRENT)
 @Tag("SampleTest")
 public class SahibindenDemo extends BaseTest
 {
     @Test
-    public void test() throws InterruptedException
+    public void test() throws InterruptedException, IOException
     {
         Thread.sleep(1000);
-        driver.get("https://www.swtestacademy.com/junit5-parallel-test-execution/");
+        driver.get("https://www.sahibinden.com/");
         Thread.sleep(10000);
+
+        takeSnapShot(driver, String.format("images/%s.png", Thread.currentThread().hashCode()));
     }
 
     @Test
-    void test2() throws InterruptedException
+    void test2() throws InterruptedException, IOException
     {
         Thread.sleep(1000);
-        driver.get("https://www.swtestacademy.com/junit5-parallel-test-execution/");
+        driver.get("https://www.sahibinden.com/");
         Thread.sleep(10000);
+
+        takeSnapShot(driver, String.format("images/%s.png", Thread.currentThread().hashCode()));
     }
 
     @Test
-    void test3() throws InterruptedException
+    void test3() throws InterruptedException, IOException
     {
         Thread.sleep(1000);
-        driver.get("https://www.swtestacademy.com/junit5-parallel-test-execution/");
+        driver.get("https://www.sahibinden.com/");
         Thread.sleep(10000);
+
+        takeSnapShot(driver, String.format("images/%s.png", Thread.currentThread().hashCode()));
     }
 
     @Test
-    void test4() throws InterruptedException
+    void test4() throws InterruptedException, IOException
     {
         Thread.sleep(1000);
-        driver.get("https://www.swtestacademy.com/junit5-parallel-test-execution/");
+        driver.get("https://www.sahibinden.com/");
         Thread.sleep(10000);
+
+        takeSnapShot(driver, String.format("images/%s.png", Thread.currentThread().hashCode()));
     }
 
     @Test
-    void test5() throws InterruptedException
+    void test5() throws InterruptedException, IOException
     {
         Thread.sleep(1000);
-        driver.get("https://www.swtestacademy.com/junit5-parallel-test-execution/");
+        driver.get("https://www.sahibinden.com/");
         Thread.sleep(10000);
+
+        takeSnapShot(driver, String.format("images/%s.png", Thread.currentThread().hashCode()));
     }
 
     @Test
-    void test6() throws InterruptedException
+    void test6() throws InterruptedException, IOException
     {
         Thread.sleep(1000);
-        driver.get("https://www.swtestacademy.com/junit5-parallel-test-execution/");
+        driver.get("https://www.sahibinden.com/");
         Thread.sleep(10000);
+
+        takeSnapShot(driver, String.format("images/%s.png", Thread.currentThread().hashCode()));
     }
 }
